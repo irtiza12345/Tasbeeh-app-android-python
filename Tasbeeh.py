d15a94e452858a6af3a5f686bbc34ae099f26413
irtiza12345 <128898015+irtiza12345@users.noreply.github.com>
2023-08-22 00:57:16 +0500

    Tasbeeh.py
    I added a button named "Reset" to the UI and bound it to the reset_counter method.
    I added the update_label method to update the label's text and save the current count to the file whenever the count changes.
    I added the load_count method to load the previous count from the file. If the file doesn't exist or doesn't contain a valid integer, it defaults to 0.
    I added the save_count method to save the current count to the file.

--- Tasbeeh.py
+++ Tasbeeh.py
@@ -4,18 +4,17 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
-
 class CounterApp(App):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.counter = 0
+        self.counter = self.load_count()  # Load previous count
 
     def build(self):
         root = BoxLayout(orientation='vertical')
 
-        label = Label(text=str(self.counter))
-        root.add_widget(label)
+        self.label = Label(text=str(self.counter))
+        root.add_widget(self.label)
 
         button_increment = Button(text='Increment')
         button_increment.bind(on_press=self.increment_counter)
@@ -25,16 +24,38 @@ class CounterApp(App):
         button_decrement.bind(on_press=self.decrement_counter)
         root.add_widget(button_decrement)
 
+        button_reset = Button(text='Reset')
+        button_reset.bind(on_press=self.reset_counter)
+        root.add_widget(button_reset)
+
         return root
 
-    def increment_counter(self):
+    def increment_counter(self, instance):
         self.counter += 1
-        label.text = str(self.counter)
+        self.update_label()
 
-    def decrement_counter(self):
+    def decrement_counter(self, instance):
         self.counter -= 1
-        label.text = str(self.counter)
+        self.update_label()
 
+    def reset_counter(self, instance):
+        self.counter = 0
+        self.update_label()
+
+    def update_label(self):
+        self.label.text = str(self.counter)
+        self.save_count()  # Save current count
+
+    def load_count(self):
+        try:
+            with open("count.txt", "r") as f:
+                return int(f.read())
+        except (FileNotFoundError, ValueError):
+            return 0
+
+    def save_count(self):
+        with open("count.txt", "w") as f:
+            f.write(str(self.counter))
 
 if __name__ == '__main__':
     CounterApp().run()
